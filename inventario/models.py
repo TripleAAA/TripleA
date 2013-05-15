6c39900721a28fec005cb60f9eae1f66e6d53814
Julio Castillo <julioandrescc@gmail.com>
2013-05-15 11:00:15 -0600

    Codigo migratorio actualizado

--- inventario/models.py
+++ inventario/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class Producto(models.Model):
-    precio = models.IntegerField(default=0)
+	precio = models.IntegerField(default=0)
 	costo = models.IntegerField(default=0)
 	nombre = models.CharField(max_length=200)
-    proveedor = models.CharField(max_length=200)
-    marca = models.CharField(max_length=200)
-    codigos = models.IntegerField(default=0)
-    especificos = models.CharField(max_length=400)
-    fecha_creacion = models.DateTimeField('date published')
-    fecha_act = models.DateTimeField('date published')
+	proveedor = models.CharField(max_length=200)
+	marca = models.CharField(max_length=200)
+	codigos = models.IntegerField(default=0)
+	especificos = models.CharField(max_length=400)
+	fecha_creacion = models.DateTimeField('date published')
+	fecha_act = models.DateTimeField('date published')
